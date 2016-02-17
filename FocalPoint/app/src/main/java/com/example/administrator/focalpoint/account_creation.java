30ee4bc6dc1076e9879a66bd82f30e8f0caabc5c
Matthew Warren <Mwarren@conestogac.on.ca>
2016-02-17 12:36:45 -0500

    Drawer layout

--- FocalPoint/app/src/main/java/com/example/administrator/focalpoint/account_creation.java
+++ FocalPoint/app/src/main/java/com/example/administrator/focalpoint/account_creation.java
@@ -2,12 +2,42 @@ package com.example.administrator.focalpoint;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class account_creation extends Activity {
+
+    private String[] menu;
+    private DrawerLayout dLayout;
+    private ListView dList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
+
+        menu = new String[]{"Login", "Courses", "Multiple Choice", "True or False", "Fill in the Blank"};
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        dList = (ListView) findViewById(R.id.left_drawer);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+
+        dList.setAdapter(adapter);
+        dList.setSelector(android.R.color.holo_orange_light);
+        dList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Clicked: " + menu[position], Toast.LENGTH_SHORT).show();
+                //dLayout.closeDrawers();
+            }
+        });
+
     }
 }
