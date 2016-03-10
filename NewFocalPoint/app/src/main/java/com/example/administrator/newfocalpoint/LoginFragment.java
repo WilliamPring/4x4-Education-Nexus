6b3e85ed63e47a0b43f8e630461a4ebd4dfa5af8
WilliamPring <mrwilliampring@gmail.com>
2016-03-10 16:42:42 -0500

    Commiting fragments

--- NewFocalPoint/app/src/main/java/com/example/administrator/newfocalpoint/LoginFragment.java
+++ NewFocalPoint/app/src/main/java/com/example/administrator/newfocalpoint/LoginFragment.java
@@ -1,14 +1,17 @@
 package com.example.administrator.newfocalpoint;
-
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
+    Button loginBttn;
+    Button createAcc;
+    Button forgotPass;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -22,9 +25,43 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        loginBttn = (Button) view.findViewById(R.id.log_in);
+        loginBttn.setOnClickListener(this);
+
+        createAcc = (Button) view.findViewById(R.id.create_account);
+        createAcc.setOnClickListener(this);
+
+        forgotPass = (Button) view.findViewById(R.id.forgot_pass);
+        forgotPass.setOnClickListener(this);
+
+        return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.log_in:
+                // do your code
+                break;
+
+            case R.id.forgot_pass:
+                // do your code
+                break;
+
+            case R.id.create_account:
+                Fragment newFragment = new LoginFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.create_account, newFragment).commit();
+                // do your code
+                break;
+
+            default:
+                break;
+        }
+    }
 }
