dfc6ffda6992da9dab2bddf440818769bdb57e43
Matt Warren <warren.matt@hotmail.com>
2016-04-13 19:13:25 -0400

    added service, no functionality

---
+++ NewFocalPoint/app/src/main/java/com/example/administrator/newfocalpoint/NewQuestionService.java
@@ -0,0 +1,22 @@
+package com.example.administrator.newfocalpoint;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NewQuestionService extends Service {
+    public NewQuestionService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
